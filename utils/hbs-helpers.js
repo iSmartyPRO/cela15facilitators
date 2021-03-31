47f1c172979d7e5666fab0a3a4f63f6f86c533c7
Ilias Aidar <Ilias@ilanapro.com>
2021-03-31 10:44:18 +0000

    Confirmation text i supdated by Zarina request

--- utils/hbs-helpers.js
+++ utils/hbs-helpers.js
@@ -5,7 +5,3 @@ module.exports.formatDate = function (date, format) {
 module.exports.ternary = function(data, ifYes, ifNo){
   return data ? ifYes : ifNo
 }
-
-function ternary(data, ifYes, ifNo){
-  return data ? ifYes : ifNo
-}
