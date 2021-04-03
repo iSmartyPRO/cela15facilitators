a18e1c5c926d2965a4be77d07e079640b208b37e
Ilias Aidar <Ilias@ilanapro.com>
2021-04-03 07:22:20 +0000

    Added displaying and sending data about Class Year and fixed bug with sendmail

--- controllers/api.js
+++ controllers/api.js
@@ -5,61 +5,61 @@ const mailer = require('../utils/mailer')
 const User = require('../models/user')
 
 function isValidDate(dateString) {
-    if(!/^\d{1,2}[.]\d{1,2}[.]\d{4}$/.test(dateString)) return false;
+    if (!/^\d{1,2}[.]\d{1,2}[.]\d{4}$/.test(dateString)) return false;
     let dateStringArr = dateString.split('.')
-    if(Number(dateStringArr[0]) < 1 || Number(dateStringArr[0]) > 31) return false;
-    if(Number(dateStringArr[1]) < 1 || Number(dateStringArr[1]) > 12) return false;
-    if(Number(dateStringArr[2]) < 1940 || Number(dateStringArr[2]) > 2015) return false;
+    if (Number(dateStringArr[0]) < 1 || Number(dateStringArr[0]) > 31) return false;
+    if (Number(dateStringArr[1]) < 1 || Number(dateStringArr[1]) > 12) return false;
+    if (Number(dateStringArr[2]) < 1940 || Number(dateStringArr[2]) > 2015) return false;
     return true;
 };
-function isValidTextArea(textareaString, wordQnty = 500){
-  if(textareaString.length < 2) return false
-  let textWords = textareaString.split(' ').length
-  if(textWords > wordQnty) return false
-  return true
+
+function isValidTextArea(textareaString, wordQnty = 500) {
+    if (textareaString.length < 2) return false
+    let textWords = textareaString.split(' ').length
+    if (textWords > wordQnty) return false
+    return true
 }
 
 function validate(data) {
     let errors = []
     let result = {}
-    // Validation
+        // Validation
     const emailRe = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    if((typeof data.fullName === 'undefined') || (data.fullName.length < 4)) { errors.push('Full Name is not valid')}
-    if((typeof data.networkName === 'undefined') || (data.networkName === "null")) { errors.push('Network name is not valid')}
-    if((typeof data.networkName === 'undefined') || (data.networkName === "null")) { errors.push('Network class year is not valid')}
-    if((typeof data.countryCitizen === 'undefined') || (data.countryCitizen === "null")) { errors.push('Country is not valid')}
-    if((typeof data.DoB === 'undefined') || (isValidDate(data.DoB) != true )) { errors.push('Date of Birth is not valid')}
-    if((typeof data.currentResidence === 'undefined') || (data.currentResidence.length < 4)) { errors.push('Current residence is not valid')}
-    if((typeof data.title === 'undefined') || (data.title.length < 2)) { errors.push('Title is not valid')}
-    if((typeof data.companyName === 'undefined') || (data.companyName.length < 2)) { errors.push('Company Name is not valid')}
-    if((typeof data.companyIndustry === 'undefined') || (data.companyIndustry.length < 2)) { errors.push('Company Industry is not valid')}
-    if((typeof data.email === 'undefined') || (emailRe.test(data.email) != true)) { errors.push('E-mail is not valid')}
-    if((typeof data.phone === 'undefined') || (data.phone.length < 2)) { errors.push('Phone is not valid')}
-    if((typeof data.bio === 'undefined') || (isValidTextArea(data.bio, 250) != true)) { errors.push(`Bio information is not valid.`)}
-    if((typeof data.isEnglish === 'undefined') || (isValidTextArea(data.isEnglish)) != true) { errors.push(`About English Information is not valid.`)}
-    if((typeof data.whyYouFacilitator === 'undefined') || (isValidTextArea(data.whyYouFacilitator)) != true) { errors.push(`Why Facilitator Information is not valid.`)}
-    if((typeof data.experienceInfo === 'undefined') || (isValidTextArea(data.experienceInfo)) != true) { errors.push(`Experience Information is not valid.`)}
-    if((typeof data.whoFacilitator === 'undefined') || (isValidTextArea(data.whoFacilitator)) != true) { errors.push(`Who Facilitator Information is not valid.`)}
-    if((typeof data.references === 'undefined') || (isValidTextArea(data.references)) != true) { errors.push(`References Information is not valid.`)}
-    if(errors.length > 0){
+    if ((typeof data.fullName === 'undefined') || (data.fullName.length < 4)) { errors.push('Full Name is not valid') }
+    if ((typeof data.networkName === 'undefined') || (data.networkName === "null")) { errors.push('Network name is not valid') }
+    if ((typeof data.networkName === 'undefined') || (data.networkName === "null")) { errors.push('Network class year is not valid') }
+    if ((typeof data.countryCitizen === 'undefined') || (data.countryCitizen === "null")) { errors.push('Country is not valid') }
+    if ((typeof data.DoB === 'undefined') || (isValidDate(data.DoB) != true)) { errors.push('Date of Birth is not valid') }
+    if ((typeof data.currentResidence === 'undefined') || (data.currentResidence.length < 4)) { errors.push('Current residence is not valid') }
+    if ((typeof data.title === 'undefined') || (data.title.length < 2)) { errors.push('Title is not valid') }
+    if ((typeof data.companyName === 'undefined') || (data.companyName.length < 2)) { errors.push('Company Name is not valid') }
+    if ((typeof data.companyIndustry === 'undefined') || (data.companyIndustry.length < 2)) { errors.push('Company Industry is not valid') }
+    if ((typeof data.email === 'undefined') || (emailRe.test(data.email) != true)) { errors.push('E-mail is not valid') }
+    if ((typeof data.phone === 'undefined') || (data.phone.length < 2)) { errors.push('Phone is not valid') }
+    if ((typeof data.bio === 'undefined') || (isValidTextArea(data.bio, 250) != true)) { errors.push(`Bio information is not valid.`) }
+    if ((typeof data.isEnglish === 'undefined') || (isValidTextArea(data.isEnglish)) != true) { errors.push(`About English Information is not valid.`) }
+    if ((typeof data.whyYouFacilitator === 'undefined') || (isValidTextArea(data.whyYouFacilitator)) != true) { errors.push(`Why Facilitator Information is not valid.`) }
+    if ((typeof data.experienceInfo === 'undefined') || (isValidTextArea(data.experienceInfo)) != true) { errors.push(`Experience Information is not valid.`) }
+    if ((typeof data.whoFacilitator === 'undefined') || (isValidTextArea(data.whoFacilitator)) != true) { errors.push(`Who Facilitator Information is not valid.`) }
+    if ((typeof data.references === 'undefined') || (isValidTextArea(data.references)) != true) { errors.push(`References Information is not valid.`) }
+    if (errors.length > 0) {
         result.status = "Bad"
         result.errors = errors
-    }
-    else {
+    } else {
         result.status = "OK"
     }
     return result
 }
 
 
-module.exports.index = async (req, res) => {
-        res.json({"status": 'ok'})
+module.exports.index = async(req, res) => {
+    res.json({ "status": 'ok' })
 }
-module.exports.store = async (req, res) => {
+module.exports.store = async(req, res) => {
     const d = req.body
-    //console.log(req.headers)
+        //console.log(req.headers)
     validationResult = validate(d)
-    if(validationResult.status === "OK"){
+    if (validationResult.status === "OK") {
         let context = {
             fullName: d.fullName,
             networkName: d.networkName,
@@ -80,21 +80,21 @@ module.exports.store = async (req, res) => {
             references: d.references,
             authPhotos: d.authPhotos === 'yes' ? true : false
         }
-       const appData = new AppForm(context)
-        try{
-            let notificationUsers = await User.find({"notification":true}).lean()
-            await appData.save(function(err, application){
+        const appData = new AppForm(context)
+        try {
+            let notificationUsers = await User.find({ "notification": true }).lean()
+            await appData.save(async function(err, application) {
                 context.id = application.id
                 context.appUrl = req.headers.host
-                for(i = 0; i < notificationUsers.length; i++){
+                for (i = 0; i < notificationUsers.length; i++) {
                     context.username = notificationUsers[i].name
-                    mailer.send(notificationUsers[i].email, 'New Application Form from ' + d.fullName, 'newAppForm', context)
+                    await mailer.send(notificationUsers[i].email, 'New Application Form from ' + d.fullName, 'newAppForm', context)
                 }
-                mailer.send(context.email, 'CELA15 Facilitators Registration', 'notificationToParticipant', context)
+                await mailer.send(context.email, 'CELA15 Facilitators Registration', 'notificationToParticipant', context)
             })
             logger.add(req, 'apiSaved')
 
-        } catch(e){
+        } catch (e) {
             console.log(e)
         }
         res.json(validationResult)
--- utils/mailer.js
+++ utils/mailer.js
@@ -2,41 +2,49 @@ const nodemailer = require('nodemailer')
 const hbs = require('nodemailer-express-handlebars')
 const config = require('../config')
 
-module.exports.send = (mailTo, mailSubject, mailTemplate, context) => {
-  let transport = nodemailer.createTransport({
-    host: config.mailHost,
-    port: 465,
-    secure: true,
-    auth: {
-      user: config.mailUser,
-      pass: config.mailPass
-    }
-  })
-  transport.use('compile', hbs({
-    viewEngine: {
-      partialsDir: __dirname + '/mailer/partials',
-      layoutsDir: __dirname + '/mailer/layouts',
-      extname: '.hbs',
-      helpers: require('./hbs-helpers')
+module.exports.send = async(mailTo, mailSubject, mailTemplate, context) => {
+    return new Promise((resolve, reject) => {
+        let transport = nodemailer.createTransport({
+            host: config.mailHost,
+            port: 465,
+            secure: true,
+            auth: {
+                user: config.mailUser,
+                pass: config.mailPass
+            }
+        })
+        transport.use('compile', hbs({
+            viewEngine: {
+                partialsDir: __dirname + '/mailer/partials',
+                layoutsDir: __dirname + '/mailer/layouts',
+                extname: '.hbs',
+                helpers: require('./hbs-helpers')
 
-    },
-    viewPath: __dirname + '/mailer/views',
-    extName: '.hbs',
-  }))
-  let message = {
-    from: config.mailFrom,
-    to: mailTo,
-    subject: ' ✔ ' + mailSubject,
-    template: mailTemplate,
-    context
-  }
+            },
+            viewPath: __dirname + '/mailer/views',
+            extName: '.hbs',
+        }))
+        message = {
+            from: config.mailFrom,
+            to: mailTo,
+            subject: ' ✔ ' + mailSubject,
+            template: mailTemplate,
+            context
+        }
+        transport.sendMail(message, async function(err, info) {
+            if (err) {
+                console.log(err)
+                resolve(false)
+            } else {
+                console.log('E-mail is send to:', mailTo + '(' + message.context.username + ')')
+                resolve(true)
+            }
+        })
 
-  transport.sendMail(message, function(err, info){
-    if(err){
-      console.log(err)
-    } else {
-      console.log('E-mail is send to:', mailTo)
-      //console.log(info)
-    }
-  })
+
+    })
+
+
+
+
 }
